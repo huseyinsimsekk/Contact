36cb5cdd5c27b4c8586c2c9f4955e8830b11dfb3
huseyinsimsekk <hsynsimsek723@gmail.com>
2022-04-09 21:40:06 +0300

    swagger prod test

--- Contact.API/Startup.cs
+++ Contact.API/Startup.cs
@@ -40,9 +40,9 @@ namespace Contact.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact.API v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contact.API v1"));
 
             app.UseHttpsRedirection();
 
