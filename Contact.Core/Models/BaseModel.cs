9396ac1e12ad51ae64ab63b4845e1b7061124314
huseyinsimsekk <hsynsimsek723@gmail.com>
2022-04-09 12:17:21 +0300

    Created Models

---
+++ Contact.Core/Models/BaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Core.Models
+{
+    public class BaseModel
+    {
+        public BaseModel()
+        {
+            IsDeleted = false;
+            CreatedDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? EffectedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Contact.Core/Models/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Core.Models
+{
+    public class Owner : BaseModel
+    {
+        public Owner()
+        {
+            Contacts = new Collection<Contact>();
+        }
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
